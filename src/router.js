be5fc17d8d8735ef7f58fb71c540a7e00b436f0a
zhanghao-web <499314316@qq.com>
2019-08-04 22:00:36 +0800

    添加登陆，401，404路由，页面

--- src/router.js
+++ src/router.js
@@ -6,6 +6,37 @@ Vue.use(Router)
 
 export const constantRoutes = [
     {
+        path: '/redirect',
+        component: Layout,
+        hidden: true,
+        children: [
+            {
+                path: '/redirect/:path*',
+                component: () => import('@/views/redirect/index')
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: () => import('@/views/login/index'),
+        hidden: true
+    },
+    {
+        path: '/auth-redirect',
+        component: () => import('@/views/login/auth-redirect'),
+        hidden: true
+    },
+    {
+        path: '/404',
+        component: () => import('@/views/error-page/404'),
+        hidden: true
+    },
+    {
+        path: '/401',
+        component: () => import('@/views/error-page/401'),
+        hidden: true
+    },
+    {
         path: '/',
         component: Layout
     },

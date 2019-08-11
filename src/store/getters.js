dfb8bce253d5472c80127b58945e732d4f8a139e
zhanghao-web <499314316@qq.com>
2019-08-11 22:21:01 +0800

    添加dashbord代码

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,10 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  permission_routes: state => state.permission.routes
+  permission_routes: state => state.permission.routes,
+  roles: state => state.user.roles,
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  introduction: state => state.user.introduction,
 }
 export default getters
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-        111
-    </div>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Dashboard'
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: { adminDashboard, editorDashboard },
+  data() {
+    return {
+      currentRole: 'adminDashboard'
     }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>

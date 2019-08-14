3f94f3000ae731c804d77caa83fb3f557c5e9512
zhanghao-web <499314316@qq.com>
2019-08-14 15:55:51 +0800

    添加mock数据

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,34 @@ const path = require('path')
 function resolve(dir) {
     return path.join(__dirname, dir)
 }
-const name = "vue Element Admin"
+const name = "vue Admin"
+const port = process.env.port || process.env.npm_config_port || 8080 // dev port
 module.exports = {
     publicPath: './', //打包后的页面相对路径
     outputDir: 'dist', //
     assetsDir: 'static',
     lintOnSave: process.env.NODE_ENV === 'development', // 在开发环境下进行eslint检查
     productionSourceMap: false,
+    devServer: {
+        port: port,
+        open: true,
+        overlay: {
+          warnings: false,
+          errors: true
+        },
+        proxy: {
+          // change xxx-api/login => mock/login
+          // detail: https://cli.vuejs.org/config/#devserver-proxy
+          [process.env.VUE_APP_BASE_API]: {
+            target: `http://127.0.0.1:${port}/mock`,
+            changeOrigin: true,
+            pathRewrite: {
+              ['^' + process.env.VUE_APP_BASE_API]: ''
+            }
+          }
+        },
+        after: require('./mock/mock-server.js')
+      },
     configureWebpack: {
         // provide the app's title in webpack's name field, so that
         // it can be accessed in index.html to inject the correct title.
